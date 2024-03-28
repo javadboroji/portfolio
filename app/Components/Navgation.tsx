f61c76889a964fed932de437945742eea455268f
javd boroji <javadboroji22222@gmail.com>
2024-03-28 23:06:40 +0330

    add font

--- app/Components/Navgation.tsx
+++ app/Components/Navgation.tsx
@@ -22,33 +22,38 @@ function Navgation() {
   }, [navgationHover])
 
   return (
-    <div className='flex items-end flex-col fixed top-[30%] right-8'>
-      <div  onMouseEnter={() => hoverItem('home')} onMouseLeave={clearState} className=' flex items-center p-4 rounded-full bg-zinc-900 hover:bg-yellow-500 mb-3'>
-        {navgationHover === 'home' ? <span data-aos={navgationHover === 'home' ?"fade-right":''}  className='font-medium text-base text-white mr-6'> خانه</span> : ''}
+    <div className='flex items-start flex-col fixed top-[30%] z-10 right-8'>
+      <div onMouseEnter={() => hoverItem('home')} onMouseLeave={clearState} className=' flex items-center p-4 rounded-full bg-zinc-900 hover:bg-yellow-500 mb-3'>
         <FaHome fontSize={21} color='#fff' />
+        {navgationHover === 'home' ? <span data-aos={navgationHover === 'home' ? "fade-left" : ''} className='font-medium text-base text-white mr-6'> خانه</span> : ''}
+
       </div>
 
       <div onMouseEnter={() => hoverItem('about')} onMouseLeave={clearState} className='flex items-center p-4 rounded-full bg-zinc-900 hover:bg-yellow-500 mb-3'>
-        {navgationHover === 'about' ? <span data-aos={navgationHover === 'about' ?"fade-right":''} className='font-medium text-base text-white mr-6'> درباره من </span> : ''}
         <FaUser fontSize={21} color='#fff' />
+        {navgationHover === 'about' ? <span data-aos={navgationHover === 'about' ? "fade-left" : ''} className='font-medium text-base text-white mr-6'> درباره من </span> : ''}
+
       </div>
 
       <div onMouseEnter={() => hoverItem('portfolio')} onMouseLeave={clearState} className=' flex items-center p-4 rounded-full bg-zinc-900 hover:bg-yellow-500 mb-3'>
-        {navgationHover === 'portfolio' ? <span data-aos={navgationHover === 'portfolio' ?"fade-right":''}  className='font-medium text-base text-white mr-6'>  گالری من </span> : ''}
         <BsBagFill fontSize={21} color='#fff' />
+        {navgationHover === 'portfolio' ? <span data-aos={navgationHover === 'portfolio' ? "fade-left" : ''} className='font-medium text-base text-white mr-6'>  گالری من </span> : ''}
+
 
       </div>
 
       <div onMouseEnter={() => hoverItem('contact')} onMouseLeave={clearState} className='flex items-center p-4 rounded-full bg-zinc-900 hover:bg-yellow-500 mb-3'>
-        {navgationHover === 'contact' ? <span data-aos={navgationHover === 'contact' ?"fade-right":''} className='font-medium text-base text-white mr-6'>   تماس با من </span> : ''}
-
         <MdEmail fontSize={21} color='#fff' />
+        {navgationHover === 'contact' ? <span data-aos={navgationHover === 'contact' ? "fade-left" : ''} className='font-medium text-base text-white mr-6'>   تماس با من </span> : ''}
+
+
       </div>
 
       <div onMouseEnter={() => hoverItem('blog')} onMouseLeave={clearState} className='flex items-center p-4 rounded-full bg-zinc-900 hover:bg-yellow-500 mb-3'>
-        {navgationHover === 'blog' ? <span data-aos={navgationHover === 'blog' ?"fade-right":''}  className='font-medium text-base text-white mr-6'> وبلاگ </span> : ''}
-
         <IoChatbubbles fontSize={21} color='#fff' />
+        {navgationHover === 'blog' ? <span data-aos={navgationHover === 'blog' ? "fade-left" : ''} className='font-medium text-base text-white mr-6'> وبلاگ </span> : ''}
+
+
       </div>
 
     </div>
