6e2c86d7af8da6513b5c88afea778e2e3fe2be34
javd boroji <javadboroji22222@gmail.com>
2024-04-08 21:15:00 +0330

    refactor/1

--- app/Components/About/Eperince.tsx
+++ app/Components/About/Eperince.tsx
@@ -30,7 +30,7 @@ function Eperince() {
                     <div className='flex items-baseline'>
                         <div className='flex p-2 bg-yellow-500 rounded-full experince-icon mx-2'><MdDataExploration color='#fff' fontSize={24} /></div>
                         <div className='flex flex-col icon-befor'>
-                            <span className='bg-slate-800 rounded-lg text-white p-2 w-fit mb-4'> از شهریور 1401 تا اکنون</span>
+                            <span className='bg-slate-800 rounded-lg text-white p-2 w-fit mb-4'> از شهریور 1402 تا اکنون</span>
                             <span className='dark:text-white mb-3'>  شرکت رسام نقش آناهیتا</span>
                             <span className='dark:text-white'>  برنامه نویس فرانت اند</span>
                         </div>
